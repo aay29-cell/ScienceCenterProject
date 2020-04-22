7b49cf72598ff1aa22f92f9f402c71c91ecbff64
Samuel Rodriguez <sar325@cornell.edu>
2020-04-22 15:48:44 -0400

    Changed the code in docking so that it's compatible with launch

--- docking.py
+++ docking.py
@@ -2,20 +2,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from inflight import *
+from launch import *
+from contants import *
+
 
 # Parameters
 m0 = 3*(10**6)# initial mass of rocket (kg) ### Will have to change
-m_rock = 0.51*(10**6) # kg mass of rocket without fuel ### Will have to change
+m_rocket = MASS_ROCKET # kg mass of rocket without fuel ### Will have to change
 r_init = 99*(10**6) # m distance away from planet during landing/docking stage ### Will have to change
 v0 = -7*10**3 # initial velocity (m/s) ### Will have to change
-m_mars = 6.39*(10**23) # mass of Mars (kg)
-r = 3.3895*(10**6) # radius of Mars (m)
-G = 6.67408*(10**-11)# gravitational constant
-# g = 9.81 # m/s^2 #earth
-# g_mars = 3.711 # m/s^2
+m_mars = float(input("What it the mass of Mars?\n ____ x 10^23> ")) * 10**23 #6.39*(10**23) = mass of Mars (kg)
+r = float(input("What is the radius of Mars?\n____x 10^6> "))*10**6 #3.3895*(10**6) = radius of Mars (m)
+G = float(input("What is the gravitational constant G?\n ____x 10^-11> "))*10**-11 #6.67408*(10**-11)= gravitational constant
 F_thrust = 4*10**7 # kg*m/s^2 Reverse thrust of rocket ### Will Have to change constant
 v_ex = -4*10**(3) # m/s velocity of ejecting fuel ### Will have to change constant
-
 
 # Equations
 def accel_grav(r):
@@ -43,7 +44,7 @@ a[0] = -accel_grav(r_init)
 ifinal = -1
 for i in range(1, timestep):
     dm = F_thrust/v_ex
-    if m[i-1] >= m_rock:
+    if m[i-1] >= m_rocket:
         m[i] = m[i-1] + dm * dt
     else:
         m[i] = m[i-1]
@@ -52,20 +53,12 @@ for i in range(1, timestep):
     v[i] = v[i-1] + a[i-1] * dt
     x[i] = x[i-1] + v[i-1] * dt
     time[i] = time[i-1] + dt
-    if x[i] <= 0:
+    if x[i-1] <=0:
         ifinal = i
         break
 
-print(ifinal)
-plt.plot(m, label='Mass', color='green')
-plt.plot(v, label='Velocity', color='blue')
-plt.legend()
-plt.show()
 
-plt.plot(a, label='Acceleration')
-plt.legend()
-plt.show()
 
-plt.plot(x, label='Position', color='black')
-plt.legend()
-plt.show()
+    plt.show()
+
+visualize(x, v, a, timestep, dt-1)
